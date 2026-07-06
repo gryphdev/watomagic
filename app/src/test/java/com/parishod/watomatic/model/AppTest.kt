adeekshith/watomatic

Expand unit test coverage from 213 to 333 tests (+56%)
Add 9 new test files covering previously untested classes:
- AppTest, ContactHolderTest, AppPackageTest (plain JUnit)
- OpenAIHelperTest, NotificationHelperTest, DbUtilsTest,
  ContactsHelperTest, AutoStartHelperTest (Robolectric)

Add edge cases to 4 existing test files:
- PreferencesManagerTest: boundary timestamps, empty sets, concurrent reset
- NotificationUtilsTest: emoji/RTL titles, null group title NPE (documents bug),
  future timestamps, actions without RemoteInput
- CustomRepliesDataTest: fresh instance get(), max-length reply
- AppUtilsTest: singleton identity, reset, empty package name

Add resetInstance() to NotificationHelper for test isolation,
consistent with PreferencesManager/AppUtils/CustomRepliesData pattern.

app/src/test/java/com/parishod/watomatic/model/AppTest.kt
@@ -1,0 +1,76 @@
+package com.parishod.watomatic.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AppTest {
+
+    @Test
+    fun `constructor stores name and packageName`() {
+        val app = App(name = "WhatsApp", packageName = "com.whatsapp")
+        assertEquals("WhatsApp", app.name)
+        assertEquals("com.whatsapp", app.packageName)
+    }
+
+    @Test
+    fun `isExperimental defaults to false`() {
+        val app = App(name = "WhatsApp", packageName = "com.whatsapp")
+        assertFalse(app.isExperimental)
+    }
+
+    @Test
+    fun `isExperimental can be set to true`() {
+        val app = App(name = "Signal", packageName = "org.signal", isExperimental = true)
+        assertTrue(app.isExperimental)
+    }
+
+    @Test
+    fun `two Apps with same values are equal`() {
+        val app1 = App("WhatsApp", "com.whatsapp", false)
+        val app2 = App("WhatsApp", "com.whatsapp", false)
+        assertEquals(app1, app2)
+        assertEquals(app1.hashCode(), app2.hashCode())
+    }
+
+    @Test
+    fun `two Apps with different names are not equal`() {
+        val app1 = App("WhatsApp", "com.whatsapp")
+        val app2 = App("Telegram", "com.whatsapp")
+        assertNotEquals(app1, app2)
+    }
+
+    @Test
+    fun `two Apps with different packageNames are not equal`() {
+        val app1 = App("WhatsApp", "com.whatsapp")
+        val app2 = App("WhatsApp", "org.telegram")
+        assertNotEquals(app1, app2)
+    }
+
+    @Test
+    fun `two Apps with different isExperimental are not equal`() {
+        val app1 = App("Signal", "org.signal", false)
+        val app2 = App("Signal", "org.signal", true)
+        assertNotEquals(app1, app2)
+    }
+
+    @Test
+    fun `copy creates modified instance`() {
+        val original = App("WhatsApp", "com.whatsapp", false)
+        val copy = original.copy(isExperimental = true)
+        assertEquals("WhatsApp", copy.name)
+        assertEquals("com.whatsapp", copy.packageName)
+        assertTrue(copy.isExperimental)
+    }
+
+    @Test
+    fun `toString includes all fields`() {
+        val app = App("WhatsApp", "com.whatsapp", true)
+        val str = app.toString()
+        assertTrue(str.contains("WhatsApp"))
+        assertTrue(str.contains("com.whatsapp"))
+        assertTrue(str.contains("true"))
+    }
+}

app/src/test/java/com/parishod/watomatic/model/logs/AppPackageTest.kt
@@ -1,0 +1,33 @@
+package com.parishod.watomatic.model.logs
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AppPackageTest {
+
+    @Test
+    fun `constructor stores packageName`() {
+        val pkg = AppPackage("com.whatsapp")
+        assertEquals("com.whatsapp", pkg.packageName)
+    }
+
+    @Test
+    fun `index defaults to 0`() {
+        val pkg = AppPackage("com.whatsapp")
+        assertEquals(0, pkg.index)
+    }
+
+    @Test
+    fun `setIndex updates value`() {
+        val pkg = AppPackage("com.whatsapp")
+        pkg.index = 42
+        assertEquals(42, pkg.index)
+    }
+
+    @Test
+    fun `setPackageName updates value`() {
+        val pkg = AppPackage("com.whatsapp")
+        pkg.packageName = "org.telegram"
+        assertEquals("org.telegram", pkg.packageName)
+    }
+}

app/src/test/java/com/parishod/watomatic/model/utils/AutoStartHelperTest.kt
@@ -1,0 +1,66 @@
+package com.parishod.watomatic.model.utils
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNotSame
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class AutoStartHelperTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    // --- getInstance ---
+
+    @Test
+    fun `getInstance returns non-null instance`() {
+        val instance = AutoStartHelper.getInstance()
+        assertNotNull(instance)
+    }
+
+    @Test
+    fun `getInstance returns new instance each time`() {
+        val first = AutoStartHelper.getInstance()
+        val second = AutoStartHelper.getInstance()
+        assertNotSame(first, second)
+    }
+
+    // --- getAutoStartPermission ---
+
+    @Test
+    fun `getAutoStartPermission does not throw for default Robolectric brand`() {
+        // Robolectric's default Build.BRAND is "robolectric" which won't match
+        // any known brand — should hit the default case and show a Toast
+        val helper = AutoStartHelper.getInstance()
+        // Should not throw; the default case shows a Toast
+        try {
+            helper.getAutoStartPermission(context)
+        } catch (e: android.content.res.Resources.NotFoundException) {
+            // Expected in Robolectric when getString() can't resolve the resource
+        }
+    }
+
+    @Test
+    @Config(qualifiers = "")
+    fun `getAutoStartPermission handles unsupported brand gracefully`() {
+        // With default Robolectric brand, should hit the default Toast case
+        val helper = AutoStartHelper.getInstance()
+        try {
+            helper.getAutoStartPermission(context)
+        } catch (e: android.content.res.Resources.NotFoundException) {
+            // Expected: getString(R.string.setting_not_available_for_device) may not resolve
+        }
+        // Test passes as long as no unexpected exception occurs
+    }
+}

app/src/test/java/com/parishod/watomatic/model/utils/ContactsHelperTest.kt
@@ -1,0 +1,125 @@
+package com.parishod.watomatic.model.utils
+
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.parishod.watomatic.model.preferences.PreferencesManager
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class ContactsHelperTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        PreferencesManager.resetInstance()
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @After
+    fun tearDown() {
+        PreferencesManager.resetInstance()
+    }
+
+    // --- getInstance ---
+
+    @Test
+    fun `getInstance returns non-null instance`() {
+        val helper = ContactsHelper.getInstance(context)
+        assertNotNull(helper)
+    }
+
+    @Test
+    fun `getInstance returns new instance each time`() {
+        val first = ContactsHelper.getInstance(context)
+        val second = ContactsHelper.getInstance(context)
+        // ContactsHelper is not a singleton — each call returns a new instance
+        assertNotNull(first)
+        assertNotNull(second)
+    }
+
+    // --- hasContactPermission ---
+
+    @Test
+    fun `hasContactPermission returns false when permission not granted`() {
+        val helper = ContactsHelper(context)
+        // By default Robolectric does not grant permissions
+        assertFalse(helper.hasContactPermission())
+    }
+
+    @Test
+    fun `hasContactPermission returns true when permission granted`() {
+        val app = context as Application
+        Shadows.shadowOf(app).grantPermissions(Manifest.permission.READ_CONTACTS)
+        val helper = ContactsHelper(context)
+        assertTrue(helper.hasContactPermission())
+    }
+
+    // --- getContactList ---
+
+    @Test
+    fun `getContactList returns empty list when no permission and no custom contacts`() {
+        val helper = ContactsHelper(context)
+        // No permission granted, no custom contacts saved
+        val result = helper.getContactList()
+        assertNotNull(result)
+        assertTrue(result.isEmpty())
+    }
+
+    @Test
+    fun `getContactList includes custom contacts from preferences`() {
+        // Save custom reply names in preferences
+        val prefs = PreferencesManager.getPreferencesInstance(context)
+        val customNames = setOf("CustomUser1", "CustomUser2")
+        prefs.customReplyNames = customNames
+
+        val helper = ContactsHelper(context)
+        val result = helper.getContactList()
+
+        // Should contain the custom contacts even without phone permission
+        assertEquals(2, result.size)
+        assertTrue(result.any { it.contactName == "CustomUser1" })
+        assertTrue(result.any { it.contactName == "CustomUser2" })
+        // Custom contacts should have isCustom = true
+        assertTrue(result.all { it.isCustom })
+    }
+
+    @Test
+    fun `getContactList returns only custom contacts when permission denied`() {
+        val prefs = PreferencesManager.getPreferencesInstance(context)
+        prefs.customReplyNames = setOf("CustomOnly")
+
+        val helper = ContactsHelper(context)
+        val result = helper.getContactList()
+
+        assertEquals(1, result.size)
+        assertEquals("CustomOnly", result[0].contactName)
+        assertTrue(result[0].isCustom)
+    }
+
+    @Test
+    fun `getContactList handles empty cursor with permission granted`() {
+        val app = context as Application
+        Shadows.shadowOf(app).grantPermissions(Manifest.permission.READ_CONTACTS)
+        val helper = ContactsHelper(context)
+
+        // With permission granted but no contacts in the provider
+        val result = helper.getContactList()
+        assertNotNull(result)
+        // May be empty since Robolectric's ContentProvider has no contacts
+        assertTrue(result.size >= 0)
+    }
+}

app/src/test/java/com/parishod/watomatic/model/utils/DbUtilsTest.kt
@@ -1,0 +1,95 @@
+package com.parishod.watomatic.model.utils
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.parishod.watomatic.model.CustomRepliesData
+import com.parishod.watomatic.model.logs.MessageLogsDB
+import com.parishod.watomatic.model.preferences.PreferencesManager
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class DbUtilsTest {
+
+    private lateinit var context: Context
+    private lateinit var dbUtils: DbUtils
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+        dbUtils = DbUtils(context)
+    }
+
+    @After
+    fun tearDown() {
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+        // Reset the database singleton via reflection
+        try {
+            val field = MessageLogsDB::class.java.getDeclaredField("_instance")
+            field.isAccessible = true
+            val db = field.get(null) as? MessageLogsDB
+            db?.close()
+            field.set(null, null)
+        } catch (_: Exception) {
+            // Ignore if reflection fails
+        }
+    }
+
+    // --- getNunReplies ---
+
+    @Test
+    fun `getNunReplies returns 0 when no logs exist`() {
+        val count = dbUtils.getNunReplies()
+        assertEquals(0L, count)
+    }
+
+    // --- getLastRepliedTime ---
+
+    @Test
+    fun `getLastRepliedTime returns 0 for null title`() {
+        val result = dbUtils.getLastRepliedTime("com.whatsapp", null)
+        assertEquals(0L, result)
+    }
+
+    @Test
+    fun `getLastRepliedTime returns 0 when no logs exist`() {
+        val result = dbUtils.getLastRepliedTime("com.whatsapp", "John")
+        assertEquals(0L, result)
+    }
+
+    // --- getFirstRepliedTime ---
+
+    @Test
+    fun `getFirstRepliedTime returns 0 when no logs exist`() {
+        val result = dbUtils.getFirstRepliedTime()
+        assertEquals(0L, result)
+    }
+
+    // --- purgeMessageLogs ---
+
+    @Test
+    fun `purgeMessageLogs does not throw when no logs exist`() {
+        // Should not throw
+        dbUtils.purgeMessageLogs()
+        assertEquals(0L, dbUtils.getNunReplies())
+    }
+
+    // --- Constructor ---
+
+    @Test
+    fun `DbUtils can be constructed with context`() {
+        val utils = DbUtils(context)
+        // Basic smoke test — doesn't crash
+        assertTrue(utils.getNunReplies() >= 0)
+    }
+}

gryphdev/watomagic

(cherry picked from commit 1490728a85f769628341411f69482affda0e1cf5)

app/src/test/java/com/parishod/watomatic/model/AppTest.kt
@@ -1,0 +1,76 @@
+package com.parishod.watomagic.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AppTest {
+
+    @Test
+    fun `constructor stores name and packageName`() {
+        val app = App(name = "WhatsApp", packageName = "com.whatsapp")
+        assertEquals("WhatsApp", app.name)
+        assertEquals("com.whatsapp", app.packageName)
+    }
+
+    @Test
+    fun `isExperimental defaults to false`() {
+        val app = App(name = "WhatsApp", packageName = "com.whatsapp")
+        assertFalse(app.isExperimental)
+    }
+
+    @Test
+    fun `isExperimental can be set to true`() {
+        val app = App(name = "Signal", packageName = "org.signal", isExperimental = true)
+        assertTrue(app.isExperimental)
+    }
+
+    @Test
+    fun `two Apps with same values are equal`() {
+        val app1 = App("WhatsApp", "com.whatsapp", false)
+        val app2 = App("WhatsApp", "com.whatsapp", false)
+        assertEquals(app1, app2)
+        assertEquals(app1.hashCode(), app2.hashCode())
+    }
+
+    @Test
+    fun `two Apps with different names are not equal`() {
+        val app1 = App("WhatsApp", "com.whatsapp")
+        val app2 = App("Telegram", "com.whatsapp")
+        assertNotEquals(app1, app2)
+    }
+
+    @Test
+    fun `two Apps with different packageNames are not equal`() {
+        val app1 = App("WhatsApp", "com.whatsapp")
+        val app2 = App("WhatsApp", "org.telegram")
+        assertNotEquals(app1, app2)
+    }
+
+    @Test
+    fun `two Apps with different isExperimental are not equal`() {
+        val app1 = App("Signal", "org.signal", false)
+        val app2 = App("Signal", "org.signal", true)
+        assertNotEquals(app1, app2)
+    }
+
+    @Test
+    fun `copy creates modified instance`() {
+        val original = App("WhatsApp", "com.whatsapp", false)
+        val copy = original.copy(isExperimental = true)
+        assertEquals("WhatsApp", copy.name)
+        assertEquals("com.whatsapp", copy.packageName)
+        assertTrue(copy.isExperimental)
+    }
+
+    @Test
+    fun `toString includes all fields`() {
+        val app = App("WhatsApp", "com.whatsapp", true)
+        val str = app.toString()
+        assertTrue(str.contains("WhatsApp"))
+        assertTrue(str.contains("com.whatsapp"))
+        assertTrue(str.contains("true"))
+    }
+}

app/src/test/java/com/parishod/watomatic/model/logs/AppPackageTest.kt
@@ -1,0 +1,33 @@
+package com.parishod.watomagic.model.logs
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AppPackageTest {
+
+    @Test
+    fun `constructor stores packageName`() {
+        val pkg = AppPackage("com.whatsapp")
+        assertEquals("com.whatsapp", pkg.packageName)
+    }
+
+    @Test
+    fun `index defaults to 0`() {
+        val pkg = AppPackage("com.whatsapp")
+        assertEquals(0, pkg.index)
+    }
+
+    @Test
+    fun `setIndex updates value`() {
+        val pkg = AppPackage("com.whatsapp")
+        pkg.index = 42
+        assertEquals(42, pkg.index)
+    }
+
+    @Test
+    fun `setPackageName updates value`() {
+        val pkg = AppPackage("com.whatsapp")
+        pkg.packageName = "org.telegram"
+        assertEquals("org.telegram", pkg.packageName)
+    }
+}

app/src/test/java/com/parishod/watomatic/model/utils/AutoStartHelperTest.kt
@@ -1,0 +1,66 @@
+package com.parishod.watomagic.model.utils
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNotSame
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class AutoStartHelperTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    // --- getInstance ---
+
+    @Test
+    fun `getInstance returns non-null instance`() {
+        val instance = AutoStartHelper.getInstance()
+        assertNotNull(instance)
+    }
+
+    @Test
+    fun `getInstance returns new instance each time`() {
+        val first = AutoStartHelper.getInstance()
+        val second = AutoStartHelper.getInstance()
+        assertNotSame(first, second)
+    }
+
+    // --- getAutoStartPermission ---
+
+    @Test
+    fun `getAutoStartPermission does not throw for default Robolectric brand`() {
+        // Robolectric's default Build.BRAND is "robolectric" which won't match
+        // any known brand — should hit the default case and show a Toast
+        val helper = AutoStartHelper.getInstance()
+        // Should not throw; the default case shows a Toast
+        try {
+            helper.getAutoStartPermission(context)
+        } catch (e: android.content.res.Resources.NotFoundException) {
+            // Expected in Robolectric when getString() can't resolve the resource
+        }
+    }
+
+    @Test
+    @Config(qualifiers = "")
+    fun `getAutoStartPermission handles unsupported brand gracefully`() {
+        // With default Robolectric brand, should hit the default Toast case
+        val helper = AutoStartHelper.getInstance()
+        try {
+            helper.getAutoStartPermission(context)
+        } catch (e: android.content.res.Resources.NotFoundException) {
+            // Expected: getString(R.string.setting_not_available_for_device) may not resolve
+        }
+        // Test passes as long as no unexpected exception occurs
+    }
+}

app/src/test/java/com/parishod/watomatic/model/utils/ContactsHelperTest.kt
@@ -1,0 +1,125 @@
+package com.parishod.watomagic.model.utils
+
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.parishod.watomagic.model.preferences.PreferencesManager
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class ContactsHelperTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        PreferencesManager.resetInstance()
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @After
+    fun tearDown() {
+        PreferencesManager.resetInstance()
+    }
+
+    // --- getInstance ---
+
+    @Test
+    fun `getInstance returns non-null instance`() {
+        val helper = ContactsHelper.getInstance(context)
+        assertNotNull(helper)
+    }
+
+    @Test
+    fun `getInstance returns new instance each time`() {
+        val first = ContactsHelper.getInstance(context)
+        val second = ContactsHelper.getInstance(context)
+        // ContactsHelper is not a singleton — each call returns a new instance
+        assertNotNull(first)
+        assertNotNull(second)
+    }
+
+    // --- hasContactPermission ---
+
+    @Test
+    fun `hasContactPermission returns false when permission not granted`() {
+        val helper = ContactsHelper(context)
+        // By default Robolectric does not grant permissions
+        assertFalse(helper.hasContactPermission())
+    }
+
+    @Test
+    fun `hasContactPermission returns true when permission granted`() {
+        val app = context as Application
+        Shadows.shadowOf(app).grantPermissions(Manifest.permission.READ_CONTACTS)
+        val helper = ContactsHelper(context)
+        assertTrue(helper.hasContactPermission())
+    }
+
+    // --- getContactList ---
+
+    @Test
+    fun `getContactList returns empty list when no permission and no custom contacts`() {
+        val helper = ContactsHelper(context)
+        // No permission granted, no custom contacts saved
+        val result = helper.getContactList()
+        assertNotNull(result)
+        assertTrue(result.isEmpty())
+    }
+
+    @Test
+    fun `getContactList includes custom contacts from preferences`() {
+        // Save custom reply names in preferences
+        val prefs = PreferencesManager.getPreferencesInstance(context)
+        val customNames = setOf("CustomUser1", "CustomUser2")
+        prefs.customReplyNames = customNames
+
+        val helper = ContactsHelper(context)
+        val result = helper.getContactList()
+
+        // Should contain the custom contacts even without phone permission
+        assertEquals(2, result.size)
+        assertTrue(result.any { it.contactName == "CustomUser1" })
+        assertTrue(result.any { it.contactName == "CustomUser2" })
+        // Custom contacts should have isCustom = true
+        assertTrue(result.all { it.isCustom })
+    }
+
+    @Test
+    fun `getContactList returns only custom contacts when permission denied`() {
+        val prefs = PreferencesManager.getPreferencesInstance(context)
+        prefs.customReplyNames = setOf("CustomOnly")
+
+        val helper = ContactsHelper(context)
+        val result = helper.getContactList()
+
+        assertEquals(1, result.size)
+        assertEquals("CustomOnly", result[0].contactName)
+        assertTrue(result[0].isCustom)
+    }
+
+    @Test
+    fun `getContactList handles empty cursor with permission granted`() {
+        val app = context as Application
+        Shadows.shadowOf(app).grantPermissions(Manifest.permission.READ_CONTACTS)
+        val helper = ContactsHelper(context)
+
+        // With permission granted but no contacts in the provider
+        val result = helper.getContactList()
+        assertNotNull(result)
+        // May be empty since Robolectric's ContentProvider has no contacts
+        assertTrue(result.size >= 0)
+    }
+}

app/src/test/java/com/parishod/watomatic/model/utils/DbUtilsTest.kt
@@ -1,0 +1,95 @@
+package com.parishod.watomagic.model.utils
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.parishod.watomagic.model.CustomRepliesData
+import com.parishod.watomagic.model.logs.MessageLogsDB
+import com.parishod.watomagic.model.preferences.PreferencesManager
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class DbUtilsTest {
+
+    private lateinit var context: Context
+    private lateinit var dbUtils: DbUtils
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+        dbUtils = DbUtils(context)
+    }
+
+    @After
+    fun tearDown() {
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+        // Reset the database singleton via reflection
+        try {
+            val field = MessageLogsDB::class.java.getDeclaredField("_instance")
+            field.isAccessible = true
+            val db = field.get(null) as? MessageLogsDB
+            db?.close()
+            field.set(null, null)
+        } catch (_: Exception) {
+            // Ignore if reflection fails
+        }
+    }
+
+    // --- getNunReplies ---
+
+    @Test
+    fun `getNunReplies returns 0 when no logs exist`() {
+        val count = dbUtils.getNunReplies()
+        assertEquals(0L, count)
+    }
+
+    // --- getLastRepliedTime ---
+
+    @Test
+    fun `getLastRepliedTime returns 0 for null title`() {
+        val result = dbUtils.getLastRepliedTime("com.whatsapp", null)
+        assertEquals(0L, result)
+    }
+
+    @Test
+    fun `getLastRepliedTime returns 0 when no logs exist`() {
+        val result = dbUtils.getLastRepliedTime("com.whatsapp", "John")
+        assertEquals(0L, result)
+    }
+
+    // --- getFirstRepliedTime ---
+
+    @Test
+    fun `getFirstRepliedTime returns 0 when no logs exist`() {
+        val result = dbUtils.getFirstRepliedTime()
+        assertEquals(0L, result)
+    }
+
+    // --- purgeMessageLogs ---
+
+    @Test
+    fun `purgeMessageLogs does not throw when no logs exist`() {
+        // Should not throw
+        dbUtils.purgeMessageLogs()
+        assertEquals(0L, dbUtils.getNunReplies())
+    }
+
+    // --- Constructor ---
+
+    @Test
+    fun `DbUtils can be constructed with context`() {
+        val utils = DbUtils(context)
+        // Basic smoke test — doesn't crash
+        assertTrue(utils.getNunReplies() >= 0)
+    }
+}