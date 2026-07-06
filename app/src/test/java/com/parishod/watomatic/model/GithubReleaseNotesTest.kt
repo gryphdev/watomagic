adeekshith/watomatic

Expand test coverage to ~90% of testable model classes; add CLAUDE.md
Tests added (202 total, 0 failures):
- NetworkModelsTest: 35 tests covering all OpenAI/Atomatic request+response POJOs
  including AtomaticAIErrorResponse.isAuthError() branch logic
- MessageLogTest: 17 tests covering Room entity constructor, getters/setters, UUID generation
- GithubReleaseNotesTest: 9 tests covering Parcelable serialization via writeToParcel/CREATOR
- NotificationUtilsTest: expanded from 12 to 21 tests, adding extractWearNotification
  scenarios (no actions, single reply action, reply-action selection priority, tag capture)
  and group title message-count trimming branch

Build config:
- Add JaCoCo plugin + jacocoUnitTestReport task for coverage reporting
- Enable enableUnitTestCoverage in debug build type

Documentation:
- Add CLAUDE.md with build instructions, test run commands, test architecture overview,
  common issues (JAVA_HOME, Robolectric resource errors, Keystore unavailability),
  and note on JaCoCo/Robolectric incompatibility with offline instrumentation

app/src/test/java/com/parishod/watomatic/model/GithubReleaseNotesTest.kt
@@ -1,0 +1,113 @@
+package com.parishod.watomatic.model
+
+import android.os.Parcel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class GithubReleaseNotesTest {
+
+    @Test
+    fun `no-arg constructor defaults all fields to null`() {
+        val notes = GithubReleaseNotes()
+        assertNull(notes.id)
+        assertNull(notes.tagName)
+        assertNull(notes.body)
+    }
+
+    @Test
+    fun `setId and getId round-trip`() {
+        val notes = GithubReleaseNotes()
+        notes.id = 12345
+        assertEquals(12345, notes.id)
+    }
+
+    @Test
+    fun `setTagName and getTagName round-trip`() {
+        val notes = GithubReleaseNotes()
+        notes.tagName = "v1.35"
+        assertEquals("v1.35", notes.tagName)
+    }
+
+    @Test
+    fun `setBody and getBody round-trip`() {
+        val notes = GithubReleaseNotes()
+        notes.body = "Bug fixes and performance improvements."
+        assertEquals("Bug fixes and performance improvements.", notes.body)
+    }
+
+    @Test
+    fun `writeToParcel and readFromParcel preserves all fields`() {
+        val original = GithubReleaseNotes()
+        original.id = 99
+        original.tagName = "v2.0"
+        original.body = "New feature release"
+
+        val parcel = Parcel.obtain()
+        original.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val restored = GithubReleaseNotes()
+        restored.readFromParcel(parcel)
+        parcel.recycle()
+
+        assertEquals(99, restored.id)
+        assertEquals("v2.0", restored.tagName)
+        assertEquals("New feature release", restored.body)
+    }
+
+    @Test
+    fun `CREATOR createFromParcel restores object`() {
+        val original = GithubReleaseNotes()
+        original.id = 77
+        original.tagName = "v1.0"
+        original.body = "Initial release"
+
+        val parcel = Parcel.obtain()
+        original.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val restored = GithubReleaseNotes.CREATOR.createFromParcel(parcel)
+        parcel.recycle()
+
+        assertEquals(77, restored.id)
+        assertEquals("v1.0", restored.tagName)
+        assertEquals("Initial release", restored.body)
+    }
+
+    @Test
+    fun `CREATOR newArray creates array of requested size`() {
+        val array = GithubReleaseNotes.CREATOR.newArray(3)
+        assertNotNull(array)
+        assertEquals(3, array.size)
+    }
+
+    @Test
+    fun `describeContents returns 0`() {
+        val notes = GithubReleaseNotes()
+        assertEquals(0, notes.describeContents())
+    }
+
+    @Test
+    fun `writeToParcel handles null fields`() {
+        val original = GithubReleaseNotes()
+        // All fields remain null
+
+        val parcel = Parcel.obtain()
+        original.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val restored = GithubReleaseNotes.CREATOR.createFromParcel(parcel)
+        parcel.recycle()
+
+        assertNull(restored.id)
+        assertNull(restored.tagName)
+        assertNull(restored.body)
+    }
+}

app/src/test/java/com/parishod/watomatic/model/logs/MessageLogTest.kt
@@ -1,0 +1,131 @@
+package com.parishod.watomatic.model.logs
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MessageLogTest {
+
+    private fun buildLog(
+        index: Int = 1,
+        title: String = "John Doe",
+        arrivedTime: Long = 1_000_000L,
+        repliedMsg: String = "I'm busy",
+        replyTime: Long = 1_001_000L
+    ) = MessageLog(index, title, arrivedTime, repliedMsg, replyTime)
+
+    @Test
+    fun `constructor stores index`() {
+        val log = buildLog(index = 7)
+        assertEquals(7, log.index)
+    }
+
+    @Test
+    fun `constructor stores notifTitle`() {
+        val log = buildLog(title = "Alice")
+        assertEquals("Alice", log.notifTitle)
+    }
+
+    @Test
+    fun `constructor stores notifArrivedTime`() {
+        val log = buildLog(arrivedTime = 9_999_000L)
+        assertEquals(9_999_000L, log.notifArrivedTime)
+    }
+
+    @Test
+    fun `constructor stores notifRepliedMsg`() {
+        val log = buildLog(repliedMsg = "Will call you back")
+        assertEquals("Will call you back", log.notifRepliedMsg)
+    }
+
+    @Test
+    fun `constructor stores notifReplyTime`() {
+        val log = buildLog(replyTime = 5_000_000L)
+        assertEquals(5_000_000L, log.notifReplyTime)
+    }
+
+    @Test
+    fun `constructor sets notifIsReplied to true`() {
+        val log = buildLog()
+        assertTrue(log.isNotifIsReplied)
+    }
+
+    @Test
+    fun `constructor generates non-null notifId`() {
+        val log = buildLog()
+        assertNotNull(log.notifId)
+        assertTrue(log.notifId.isNotEmpty())
+    }
+
+    @Test
+    fun `each MessageLog gets a unique notifId`() {
+        val log1 = buildLog()
+        val log2 = buildLog()
+        assertTrue(log1.notifId != log2.notifId)
+    }
+
+    @Test
+    fun `setId persists value`() {
+        val log = buildLog()
+        log.id = 42
+        assertEquals(42, log.id)
+    }
+
+    @Test
+    fun `setIndex persists value`() {
+        val log = buildLog(index = 1)
+        log.index = 99
+        assertEquals(99, log.index)
+    }
+
+    @Test
+    fun `setNotifId persists value`() {
+        val log = buildLog()
+        log.notifId = "custom-uuid"
+        assertEquals("custom-uuid", log.notifId)
+    }
+
+    @Test
+    fun `setNotifTitle persists value`() {
+        val log = buildLog(title = "original")
+        log.notifTitle = "updated"
+        assertEquals("updated", log.notifTitle)
+    }
+
+    @Test
+    fun `setNotifArrivedTime persists value`() {
+        val log = buildLog(arrivedTime = 0L)
+        log.notifArrivedTime = 12_345_678L
+        assertEquals(12_345_678L, log.notifArrivedTime)
+    }
+
+    @Test
+    fun `setNotifIsReplied persists false`() {
+        val log = buildLog()
+        log.isNotifIsReplied = false
+        assertEquals(false, log.isNotifIsReplied)
+    }
+
+    @Test
+    fun `setNotifRepliedMsg persists value`() {
+        val log = buildLog(repliedMsg = "original reply")
+        log.notifRepliedMsg = "updated reply"
+        assertEquals("updated reply", log.notifRepliedMsg)
+    }
+
+    @Test
+    fun `setNotifReplyTime persists value`() {
+        val log = buildLog(replyTime = 0L)
+        log.notifReplyTime = 9_876_543L
+        assertEquals(9_876_543L, log.notifReplyTime)
+    }
+
+    @Test
+    fun `notifId is a valid UUID format`() {
+        val log = buildLog()
+        // UUID format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        val uuidRegex = Regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$")
+        assertTrue(log.notifId.matches(uuidRegex))
+    }
+}

gryphdev/watomagic

(cherry picked from commit 2e33d7f291ff85434e7b2ee5e7727e5ed8d46231)

app/src/test/java/com/parishod/watomatic/model/GithubReleaseNotesTest.kt
@@ -1,0 +1,113 @@
+package com.parishod.watomagic.model
+
+import android.os.Parcel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class GithubReleaseNotesTest {
+
+    @Test
+    fun `no-arg constructor defaults all fields to null`() {
+        val notes = GithubReleaseNotes()
+        assertNull(notes.id)
+        assertNull(notes.tagName)
+        assertNull(notes.body)
+    }
+
+    @Test
+    fun `setId and getId round-trip`() {
+        val notes = GithubReleaseNotes()
+        notes.id = 12345
+        assertEquals(12345, notes.id)
+    }
+
+    @Test
+    fun `setTagName and getTagName round-trip`() {
+        val notes = GithubReleaseNotes()
+        notes.tagName = "v1.35"
+        assertEquals("v1.35", notes.tagName)
+    }
+
+    @Test
+    fun `setBody and getBody round-trip`() {
+        val notes = GithubReleaseNotes()
+        notes.body = "Bug fixes and performance improvements."
+        assertEquals("Bug fixes and performance improvements.", notes.body)
+    }
+
+    @Test
+    fun `writeToParcel and readFromParcel preserves all fields`() {
+        val original = GithubReleaseNotes()
+        original.id = 99
+        original.tagName = "v2.0"
+        original.body = "New feature release"
+
+        val parcel = Parcel.obtain()
+        original.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val restored = GithubReleaseNotes()
+        restored.readFromParcel(parcel)
+        parcel.recycle()
+
+        assertEquals(99, restored.id)
+        assertEquals("v2.0", restored.tagName)
+        assertEquals("New feature release", restored.body)
+    }
+
+    @Test
+    fun `CREATOR createFromParcel restores object`() {
+        val original = GithubReleaseNotes()
+        original.id = 77
+        original.tagName = "v1.0"
+        original.body = "Initial release"
+
+        val parcel = Parcel.obtain()
+        original.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val restored = GithubReleaseNotes.CREATOR.createFromParcel(parcel)
+        parcel.recycle()
+
+        assertEquals(77, restored.id)
+        assertEquals("v1.0", restored.tagName)
+        assertEquals("Initial release", restored.body)
+    }
+
+    @Test
+    fun `CREATOR newArray creates array of requested size`() {
+        val array = GithubReleaseNotes.CREATOR.newArray(3)
+        assertNotNull(array)
+        assertEquals(3, array.size)
+    }
+
+    @Test
+    fun `describeContents returns 0`() {
+        val notes = GithubReleaseNotes()
+        assertEquals(0, notes.describeContents())
+    }
+
+    @Test
+    fun `writeToParcel handles null fields`() {
+        val original = GithubReleaseNotes()
+        // All fields remain null
+
+        val parcel = Parcel.obtain()
+        original.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val restored = GithubReleaseNotes.CREATOR.createFromParcel(parcel)
+        parcel.recycle()
+
+        assertNull(restored.id)
+        assertNull(restored.tagName)
+        assertNull(restored.body)
+    }
+}

app/src/test/java/com/parishod/watomatic/model/logs/MessageLogTest.kt
@@ -1,0 +1,131 @@
+package com.parishod.watomagic.model.logs
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MessageLogTest {
+
+    private fun buildLog(
+        index: Int = 1,
+        title: String = "John Doe",
+        arrivedTime: Long = 1_000_000L,
+        repliedMsg: String = "I'm busy",
+        replyTime: Long = 1_001_000L
+    ) = MessageLog(index, title, arrivedTime, repliedMsg, replyTime)
+
+    @Test
+    fun `constructor stores index`() {
+        val log = buildLog(index = 7)
+        assertEquals(7, log.index)
+    }
+
+    @Test
+    fun `constructor stores notifTitle`() {
+        val log = buildLog(title = "Alice")
+        assertEquals("Alice", log.notifTitle)
+    }
+
+    @Test
+    fun `constructor stores notifArrivedTime`() {
+        val log = buildLog(arrivedTime = 9_999_000L)
+        assertEquals(9_999_000L, log.notifArrivedTime)
+    }
+
+    @Test
+    fun `constructor stores notifRepliedMsg`() {
+        val log = buildLog(repliedMsg = "Will call you back")
+        assertEquals("Will call you back", log.notifRepliedMsg)
+    }
+
+    @Test
+    fun `constructor stores notifReplyTime`() {
+        val log = buildLog(replyTime = 5_000_000L)
+        assertEquals(5_000_000L, log.notifReplyTime)
+    }
+
+    @Test
+    fun `constructor sets notifIsReplied to true`() {
+        val log = buildLog()
+        assertTrue(log.isNotifIsReplied)
+    }
+
+    @Test
+    fun `constructor generates non-null notifId`() {
+        val log = buildLog()
+        assertNotNull(log.notifId)
+        assertTrue(log.notifId.isNotEmpty())
+    }
+
+    @Test
+    fun `each MessageLog gets a unique notifId`() {
+        val log1 = buildLog()
+        val log2 = buildLog()
+        assertTrue(log1.notifId != log2.notifId)
+    }
+
+    @Test
+    fun `setId persists value`() {
+        val log = buildLog()
+        log.id = 42
+        assertEquals(42, log.id)
+    }
+
+    @Test
+    fun `setIndex persists value`() {
+        val log = buildLog(index = 1)
+        log.index = 99
+        assertEquals(99, log.index)
+    }
+
+    @Test
+    fun `setNotifId persists value`() {
+        val log = buildLog()
+        log.notifId = "custom-uuid"
+        assertEquals("custom-uuid", log.notifId)
+    }
+
+    @Test
+    fun `setNotifTitle persists value`() {
+        val log = buildLog(title = "original")
+        log.notifTitle = "updated"
+        assertEquals("updated", log.notifTitle)
+    }
+
+    @Test
+    fun `setNotifArrivedTime persists value`() {
+        val log = buildLog(arrivedTime = 0L)
+        log.notifArrivedTime = 12_345_678L
+        assertEquals(12_345_678L, log.notifArrivedTime)
+    }
+
+    @Test
+    fun `setNotifIsReplied persists false`() {
+        val log = buildLog()
+        log.isNotifIsReplied = false
+        assertEquals(false, log.isNotifIsReplied)
+    }
+
+    @Test
+    fun `setNotifRepliedMsg persists value`() {
+        val log = buildLog(repliedMsg = "original reply")
+        log.notifRepliedMsg = "updated reply"
+        assertEquals("updated reply", log.notifRepliedMsg)
+    }
+
+    @Test
+    fun `setNotifReplyTime persists value`() {
+        val log = buildLog(replyTime = 0L)
+        log.notifReplyTime = 9_876_543L
+        assertEquals(9_876_543L, log.notifReplyTime)
+    }
+
+    @Test
+    fun `notifId is a valid UUID format`() {
+        val log = buildLog()
+        // UUID format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        val uuidRegex = Regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$")
+        assertTrue(log.notifId.matches(uuidRegex))
+    }
+}