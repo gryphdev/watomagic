adeekshith/watomatic

Expand instrumentation test coverage from 24 to 82 tests (+242%)
Add 5 new instrumentation test files:
- SettingsActivityTest: launch, toolbar, fragment container, up button
- AboutActivityTest: launch, version display, privacy/developer links
- DonationActivityTest: launch, root layout, fragment container
- CustomReplyEditorActivityTest: reply method cards, save button, toolbar
- CustomRepliesDataInstrumentedTest: real-device SharedPreferences persistence

Expand MainActivityTest with bottom nav items, filter description
visibility/content checks, and proper state isolation in setUp/tearDown.

app/src/androidTest/java/com/parishod/watomatic/AboutActivityTest.kt
@@ -1,0 +1,95 @@
+package com.parishod.watomatic
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.parishod.watomatic.activity.about.AboutActivity
+import org.hamcrest.Matchers.containsString
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented tests for [AboutActivity].
+ *
+ * Verifies the about screen displays version info, privacy policy link,
+ * and developer link.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AboutActivityTest {
+
+    private fun launchActivity(): ActivityScenario<AboutActivity> {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), AboutActivity::class.java)
+        return ActivityScenario.launch(intent)
+    }
+
+    @Test
+    fun aboutActivityLaunchesSuccessfully() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun appVersionIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.appVersion)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun appVersionContainsVersionName() {
+        val scenario = launchActivity()
+        onView(withId(R.id.appVersion))
+            .check(matches(withText(containsString(BuildConfig.VERSION_NAME))))
+        scenario.close()
+    }
+
+    @Test
+    fun privacyPolicyCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.privacyPolicyCardView)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun developerLinkIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.developerLink)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun scrollViewIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.about_scroll_view)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun appTitleCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.appTitleCardView)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun activityCanBeRecreated() {
+        val scenario = launchActivity()
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+}

app/src/androidTest/java/com/parishod/watomatic/CustomReplyEditorActivityTest.kt
@@ -1,0 +1,144 @@
+package com.parishod.watomatic
+
+import android.content.Intent
+import androidx.preference.PreferenceManager
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.parishod.watomatic.activity.customreplyeditor.CustomReplyEditorActivity
+import com.parishod.watomatic.model.CustomRepliesData
+import com.parishod.watomatic.model.preferences.PreferencesManager
+import org.junit.After
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented tests for [CustomReplyEditorActivity].
+ *
+ * Verifies the reply editor screen launches correctly and displays the
+ * three reply method cards (manual, automatic AI, BYOK) along with the
+ * save button and other key UI elements.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class CustomReplyEditorActivityTest {
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit()
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+    }
+
+    @After
+    fun tearDown() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit()
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+    }
+
+    private fun launchActivity(): ActivityScenario<CustomReplyEditorActivity> {
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            CustomReplyEditorActivity::class.java
+        )
+        return ActivityScenario.launch(intent)
+    }
+
+    @Test
+    fun activityLaunchesSuccessfully() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun toolbarIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun manualRepliesCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.manual_replies_card)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun automaticAiProviderCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.automatic_ai_provider_card))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun otherAiProviderCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.other_ai_provider_card))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun saveButtonIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.saveCustomReplyBtn)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun scrollViewIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.custom_reply_editor_scroll_view)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun communityTemplatesLinkExists() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            val view = activity.findViewById<android.view.View>(R.id.tip_wato_message)
+            assertNotNull("Community templates link should exist", view)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun activityCanBeRecreated() {
+        val scenario = launchActivity()
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun manualReplyTextInputExists() {
+        val scenario = launchActivity()
+        // Manual is the default selected method, so the text input should exist
+        scenario.onActivity { activity ->
+            val view = activity.findViewById<android.view.View>(R.id.autoReplyTextInputEditText)
+            assertNotNull("Manual reply text input should exist", view)
+        }
+        scenario.close()
+    }
+}

app/src/androidTest/java/com/parishod/watomatic/SettingsActivityTest.kt
@@ -1,0 +1,75 @@
+package com.parishod.watomatic
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.parishod.watomatic.activity.settings.SettingsActivity
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented tests for [SettingsActivity].
+ *
+ * Verifies that the activity launches correctly, displays the toolbar and
+ * settings fragment container, and survives configuration changes.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class SettingsActivityTest {
+
+    private fun launchActivity(): ActivityScenario<SettingsActivity> {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), SettingsActivity::class.java)
+        return ActivityScenario.launch(intent)
+    }
+
+    @Test
+    fun settingsActivityLaunchesSuccessfully() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun toolbarIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun settingsFragmentContainerIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.setting_fragment_container)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun activityCanBeRecreated() {
+        val scenario = launchActivity()
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun supportActionBarHasUpButton() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity.supportActionBar)
+            assert(activity.supportActionBar!!.displayOptions and
+                    androidx.appcompat.app.ActionBar.DISPLAY_HOME_AS_UP != 0)
+        }
+        scenario.close()
+    }
+}

gryphdev/watomagic

(cherry picked from commit e133cace90fc746c6ff81edb24b24869becfafdc)

app/src/androidTest/java/com/parishod/watomatic/AboutActivityTest.kt
@@ -1,0 +1,95 @@
+package com.parishod.watomagic
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.parishod.watomagic.activity.about.AboutActivity
+import org.hamcrest.Matchers.containsString
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented tests for [AboutActivity].
+ *
+ * Verifies the about screen displays version info, privacy policy link,
+ * and developer link.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AboutActivityTest {
+
+    private fun launchActivity(): ActivityScenario<AboutActivity> {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), AboutActivity::class.java)
+        return ActivityScenario.launch(intent)
+    }
+
+    @Test
+    fun aboutActivityLaunchesSuccessfully() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun appVersionIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.appVersion)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun appVersionContainsVersionName() {
+        val scenario = launchActivity()
+        onView(withId(R.id.appVersion))
+            .check(matches(withText(containsString(BuildConfig.VERSION_NAME))))
+        scenario.close()
+    }
+
+    @Test
+    fun privacyPolicyCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.privacyPolicyCardView)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun developerLinkIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.developerLink)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun scrollViewIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.about_scroll_view)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun appTitleCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.appTitleCardView)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun activityCanBeRecreated() {
+        val scenario = launchActivity()
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+}

app/src/androidTest/java/com/parishod/watomatic/CustomReplyEditorActivityTest.kt
@@ -1,0 +1,144 @@
+package com.parishod.watomagic
+
+import android.content.Intent
+import androidx.preference.PreferenceManager
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.parishod.watomagic.activity.customreplyeditor.CustomReplyEditorActivity
+import com.parishod.watomagic.model.CustomRepliesData
+import com.parishod.watomagic.model.preferences.PreferencesManager
+import org.junit.After
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented tests for [CustomReplyEditorActivity].
+ *
+ * Verifies the reply editor screen launches correctly and displays the
+ * three reply method cards (manual, automatic AI, BYOK) along with the
+ * save button and other key UI elements.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class CustomReplyEditorActivityTest {
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit()
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+    }
+
+    @After
+    fun tearDown() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit()
+        PreferencesManager.resetInstance()
+        CustomRepliesData.resetInstance()
+    }
+
+    private fun launchActivity(): ActivityScenario<CustomReplyEditorActivity> {
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            CustomReplyEditorActivity::class.java
+        )
+        return ActivityScenario.launch(intent)
+    }
+
+    @Test
+    fun activityLaunchesSuccessfully() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun toolbarIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun manualRepliesCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.manual_replies_card)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun automaticAiProviderCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.automatic_ai_provider_card))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun otherAiProviderCardIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.other_ai_provider_card))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun saveButtonIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.saveCustomReplyBtn)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun scrollViewIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.custom_reply_editor_scroll_view)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun communityTemplatesLinkExists() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            val view = activity.findViewById<android.view.View>(R.id.tip_wato_message)
+            assertNotNull("Community templates link should exist", view)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun activityCanBeRecreated() {
+        val scenario = launchActivity()
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun manualReplyTextInputExists() {
+        val scenario = launchActivity()
+        // Manual is the default selected method, so the text input should exist
+        scenario.onActivity { activity ->
+            val view = activity.findViewById<android.view.View>(R.id.autoReplyTextInputEditText)
+            assertNotNull("Manual reply text input should exist", view)
+        }
+        scenario.close()
+    }
+}

app/src/androidTest/java/com/parishod/watomatic/SettingsActivityTest.kt
@@ -1,0 +1,75 @@
+package com.parishod.watomagic
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.parishod.watomagic.activity.settings.SettingsActivity
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented tests for [SettingsActivity].
+ *
+ * Verifies that the activity launches correctly, displays the toolbar and
+ * settings fragment container, and survives configuration changes.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class SettingsActivityTest {
+
+    private fun launchActivity(): ActivityScenario<SettingsActivity> {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), SettingsActivity::class.java)
+        return ActivityScenario.launch(intent)
+    }
+
+    @Test
+    fun settingsActivityLaunchesSuccessfully() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun toolbarIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun settingsFragmentContainerIsDisplayed() {
+        val scenario = launchActivity()
+        onView(withId(R.id.setting_fragment_container)).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
+    @Test
+    fun activityCanBeRecreated() {
+        val scenario = launchActivity()
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            assertNotNull(activity)
+        }
+        scenario.close()
+    }
+
+    @Test
+    fun supportActionBarHasUpButton() {
+        val scenario = launchActivity()
+        scenario.onActivity { activity ->
+            assertNotNull(activity.supportActionBar)
+            assert(activity.supportActionBar!!.displayOptions and
+                    androidx.appcompat.app.ActionBar.DISPLAY_HOME_AS_UP != 0)
+        }
+        scenario.close()
+    }
+}